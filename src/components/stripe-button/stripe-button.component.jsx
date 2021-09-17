283329138721a738839fd3a529b0360fb38fa540
Raúl Gerardo Vázquez Perdomo <rvazquezp@axtel.com.mx>
2021-09-17 09:50:16 -0500

    updated commit

--- src/components/stripe-button/stripe-button.component.jsx
+++ src/components/stripe-button/stripe-button.component.jsx
@@ -1,29 +1,30 @@
-import React from 'react';
-import StripeCheckout from 'react-stripe-checkout';
+import React from "react";
+import StripeCheckout from "react-stripe-checkout";
 
 const StripeCheckoutButton = ({ price }) => {
-  const priceForStripe = price * 100;
-  const publishableKey = 'pk_test_WBqax2FWVzS9QlpJScO07iuL';
+   const priceForStripe = price * 100;
+   const publishableKey =
+      "pk_test_51IfxWbGBFKzQA2LfP9F9botOEdFrHSDKyOpYjqkNzp0qKrU0rBIparzJORFS4PvCri93iI30NxoEmTMdOBZX0JyV00gNQrfJvJ";
 
-  const onToken = token => {
-    console.log(token);
-    alert('Payment Succesful!');
-  };
+   const onToken = (token) => {
+      console.log(token);
+      alert("Payment Succesful!");
+   };
 
-  return (
-    <StripeCheckout
-      label='Pay Now'
-      name='CRWN Clothing Ltd.'
-      billingAddress
-      shippingAddress
-      image='https://svgshare.com/i/CUz.svg'
-      description={`Your total is $${price}`}
-      amount={priceForStripe}
-      panelLabel='Pay Now'
-      token={onToken}
-      stripeKey={publishableKey}
-    />
-  );
+   return (
+      <StripeCheckout
+         label="Pay Now"
+         name="CRWN Clothing Ltd."
+         billingAddress
+         shippingAddress
+         image="https://svgshare.com/i/CUz.svg"
+         description={`Your total is $${price}`}
+         amount={priceForStripe}
+         panelLabel="Pay Now"
+         token={onToken}
+         stripeKey={publishableKey}
+      />
+   );
 };
 
 export default StripeCheckoutButton;
